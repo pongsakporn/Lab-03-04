4beb1729279c266318cb148e241e12d39044f13c
pongsakorn <6210110211@psu.ac.th>
2022-08-20 17:17:21 +0700

    commit3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
 import WordCard from './WordCard';
-import countDown from './Timedown';
-const word = "kub";
+
+const word = "Hello";
+
 function App() {
+  countDown(60);
     return (
+      
       <div>
         
-          Timedown(15);
-          <WordCard value = {'kub'}/>
+        
+          <WordCard value = {'Hello'}/>
+          
   
       </div>
     );
+}
+function countDown(secs){
+  
+  let id =setTimeout(()=>{
+    countDown(secs-1)
+  },1000);
+  
+  if(secs ===0){
+     clearTimeout(id);
+     console.log("หมดเวลา..");
+  }else{
+     console.log('+secs+')
+  }
+   
 }
 
 export default App;
